bbf296d6ce7534979bfd58d6acade15e6f69d718
Venus Vmd <Venus Vmd@DESKTOP-NBBTJMV>
2025-09-11 22:31:10 +0530

    datatypes

---
+++ tns/src/com/example/Datatypes.java
@@ -0,0 +1,24 @@
+package com.example;
+public class Datatypes {
+    public static void main(String[] args) {
+        byte byteVar = 100;
+        short shortVar = 30000;
+        int intVar = 100000;
+        long longVar = 10000000000L;
+        float floatVar = 3.14f;
+        double doubleVar = 3.14159265359;
+        char charVar = 'A';
+        boolean boolVar = true;
+        String stringVar = "Hello, Java!";
+
+        System.out.println("Byte Value: " + byteVar);
+        System.out.println("Short Value: " + shortVar);
+        System.out.println("Int Value: " + intVar);
+        System.out.println("Long Value: " + longVar);
+        System.out.println("Float Value: " + floatVar);
+        System.out.println("Double Value: " + doubleVar);
+        System.out.println("Char Value: " + charVar);
+        System.out.println("Boolean Value: " + boolVar);
+        System.out.println("String Value: " + stringVar);
+    }
+}
