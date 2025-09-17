c95344a68234343316ddf298c9b81c0c3e3df50d
Venus Vmd <Venus Vmd@DESKTOP-NBBTJMV>
2025-09-17 22:30:28 +0530

    variables

---
+++ tns/src/com/example/variables.java
@@ -0,0 +1,18 @@
+package com.example;
+
+public class variables {
+       String name= "Shravya";
+       int age=20;
+       public void Details() {
+    	   int year=2005;
+    	   System.out.println(name);
+    	   System.out.println(age);
+    	   System.out.println(year);
+    	   }
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+       variables d=new variables();
+       d.Details();
+	}
+
+}
