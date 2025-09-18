fb611c23560bc00defea7ec46d3ec8f5382f02d0
Venus Vmd <Venus Vmd@DESKTOP-NBBTJMV>
2025-09-18 23:25:37 +0530

    AbstractDemo

---
+++ tns/src/com/day8/AbstractDemo.java
@@ -0,0 +1,15 @@
+package com.day8;
+
+public class AbstractDemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+Square sq=new Square(5);
+sq.calArea();
+sq.show();
+Rectangle r=new Rectangle(9,4);
+r.calArea();
+r.show();
+	}
+
+}
