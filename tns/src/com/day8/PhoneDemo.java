f9254571990200ce442f1deaa02737a7eee79e25
Venus Vmd <Venus Vmd@DESKTOP-NBBTJMV>
2025-09-18 23:28:43 +0530

    PhoneDemo

---
+++ tns/src/com/day8/PhoneDemo.java
@@ -0,0 +1,14 @@
+package com.day8;
+
+public class PhoneDemo {
+
+	public static void main(String[] args) {
+		jio j=new jio();
+		j.call();
+		j.sms();
+		samsung s=new samsung();
+		s.call();
+		s.sms();
+	}
+
+}
