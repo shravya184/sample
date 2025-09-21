bb7085709c3810fe145be71868a409bb28ebb488
Venus Vmd <Venus Vmd@DESKTOP-NBBTJMV>
2025-09-21 21:48:55 +0530

    InterfaceExample

---
+++ tns/src/com/day9/InterfaceExample.java
@@ -0,0 +1,11 @@
+package com.day9;
+
+public class InterfaceExample {
+	 public static void main(String[] args) {
+	        Car myCar = new Sedan();
+	        myCar.start();
+	        myCar.openTrunk();
+	        myCar.stop();
+	    }
+	}
+
