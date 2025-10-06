37e5eae124e893347fdf8cd8befaa5ca05a56c72
Venus Vmd <Venus Vmd@DESKTOP-NBBTJMV>
2025-10-06 22:27:02 +0530

    Runnabledemo

---
+++ tns/src/notes/RunnableDemo.java
@@ -0,0 +1,11 @@
+package notes;
+
+public class RunnableDemo {
+
+	public static void main(String[] args) {
+		        Runnableinf r1 = new Runnableinf();
+		        Thread t = new Thread(r1);
+		        t.start();
+		    }
+		}
+	
