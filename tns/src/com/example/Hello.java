8401d675f141361f39ce35a09fb2525258e6953d
Venus Vmd <Venus Vmd@DESKTOP-NBBTJMV>
2025-10-06 22:18:45 +0530

    Hello

---
+++ tns/src/com/example/Hello.java
@@ -0,0 +1,11 @@
+package com.example;
+
+public class Hello {
+	    int x = 20;
+	    public void demo() {
+	        int x = 30;
+	        System.out.println(x); 
+	        System.out.println(this.x); 
+	    }
+	}
+	
