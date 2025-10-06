81c2d2f6bf47059756e102985c7fbde9d766a29a
Venus Vmd <Venus Vmd@DESKTOP-NBBTJMV>
2025-10-06 22:10:19 +0530

    single

---
+++ tns/src/com/example/Single.java
@@ -0,0 +1,15 @@
+package com.example;
+
+public class Single{
+		int a=10;
+		public void show()
+		{
+		System.out.println("This is class A");
+		}
+}
+		class B extends Single{
+		public void display()
+		{
+		System.out.println("This is class B");
+		}
+		}
