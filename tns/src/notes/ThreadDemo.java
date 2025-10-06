ed477e687ddcbec1a0abb07ede1e9fbb078e38d9
Venus Vmd <Venus Vmd@DESKTOP-NBBTJMV>
2025-10-06 22:24:32 +0530

    ThreadDemo

---
+++ tns/src/notes/ThreadDemo.java
@@ -0,0 +1,14 @@
+package notes;
+	public class ThreadDemo extends Thread {
+	    public void run() {
+	        for (int i=1; i<=10; i++) {
+	            try {
+	                System.out.println("This is thread");
+	                System.out.println(Thread.currentThread());
+	                Thread.sleep(2000);
+	            } catch (Exception e) {
+	                System.out.println(e);
+	            }
+	        }
+	    }
+	}
