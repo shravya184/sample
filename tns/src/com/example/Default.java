8705e0d2aa7beea7cc7b99813104726cbe7967be
Venus Vmd <Venus Vmd@DESKTOP-NBBTJMV>
2025-10-06 22:20:09 +0530

    Default Constructor

---
+++ tns/src/com/example/Default.java
@@ -0,0 +1,13 @@
+package com.example;
+
+public class Default {
+	    int x=1, y=2, z=3;
+	    Default() {
+	    	System.out.println("This is constructor");
+	    }
+	    public void show() {
+	        int volume = x*y*z;
+	        System.out.println(volume);
+	    }
+	}
+	
